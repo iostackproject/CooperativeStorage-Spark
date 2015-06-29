68cceb47a2801a7ca33b1d19dd040557d1950dd8
genis ortiz <genis.ortiz.gomis@gmail.com>
2015-06-29 10:32:30 +0200

    Delete uncompilable version 2

--- spark-1.4.0/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Compile and build Spark'
-
-build/mvn -Pyarn -Phadoop-2.6 -Dhadoop.version=2.6.0 -DskipTests clean package
-
-./make-distribution.sh --name custom-spark --tgz -Phadoop-2.6 -Pyarn
-
-echo 'Completed compilation'
