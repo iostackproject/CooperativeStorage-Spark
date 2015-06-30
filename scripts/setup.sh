fdd6d2435bed94c647a359dac61e618b6f4ae066
genis ortiz <genis.ortiz.gomis@gmail.com>
2015-06-30 10:30:27 +0200

    Improved setup, gitignore and added compile script for submodule spark

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "export JAVA_HOME='/usr/lib/jvm/java-1.7.0-openjdk-amd64'" >> /home/vagrant/.bashrc
+old_export=`grep -n "export JAVA_HOME" ~/.bashrc | cut -d : -f 1 | sort -r` 
+
+for i in $old_export; do
+	sed -i ""$i"d" ~/.bashrc
+done
+
+echo "export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64'" >> ~/.bashrc
+
